c17d4ec6aca061602a9dbee8bcd4d2bd280fb158
dark-noob83 <mahdiamr83@gmail.com>
2022-11-13 22:05:10 +0330

    button sum and -

--- first.py
+++ first.py
@@ -2,6 +2,15 @@ from tkinter  import *
 root=Tk()
 root.geometry("300x400")
 
+def Sum ():
+    a.delete(0,END)
+    a.insert(END,int(enterNum1.get())+int(enterNum2.get()))
+    
+def mine ():
+    a.delete(0,END)
+    a.insert(END,int(enterNum1.get())-int(enterNum2.get()))
+    
+
 Label(root,text='enterNum1 :').pack()
 enterNum1 = Entry(root)
 
@@ -9,6 +18,11 @@ enterNum1.pack()
 Label(root,text='enterNum1 :').pack()
 enterNum2 = Entry(root)
 enterNum2.pack()
-
+s=Button(root , text='+',command=Sum)
+s.pack()
+m=Button(root , text='-',command=mine)
+m.pack()
+a=Listbox(root)
+a.pack()
 
 root.mainloop()
