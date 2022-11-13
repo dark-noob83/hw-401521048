d038ab7b7ecc9c66de7fba6d4741bce07cdcedc0
dark-noob83 <mahdiamr83@gmail.com>
2022-11-13 21:57:55 +0330

    create place for enter number

--- first.py
+++ first.py
@@ -1,7 +1,14 @@
 from tkinter  import *
 root=Tk()
+root.geometry("300x400")
 
+Label(root,text='enterNum1 :').pack()
+enterNum1 = Entry(root)
 
+enterNum1.pack()
+Label(root,text='enterNum1 :').pack()
+enterNum2 = Entry(root)
+enterNum2.pack()
 
 
 root.mainloop()
