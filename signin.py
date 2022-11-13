9e0c85cca3ea7405adb058e91a36332c95e09795
dark-noob83 <mahdiamr83@gmail.com>
2022-11-13 22:22:40 +0330

    first Name and last Name

--- signin.py
+++ signin.py
@@ -1,5 +1,16 @@
 from tkinter import *
+
+
 root = Tk()
 root.geometry("300x400")
+
+
+
+Label(root,text="Name : ").pack()
+name=Entry(root)
+name.pack()
+Label(root,text="Family : ").pack()
+family=Entry(root)
+family.pack()
 
 root.mainloop()
